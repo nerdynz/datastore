b1bc37b4f97e206a39f246e7f5b8bb367c83f028
Joshua Cave <josh@insite.ai>
2021-09-25 10:45:20 +1200

    PGX dead end

--- datastore.go
+++ datastore.go
@@ -1,23 +1,15 @@
 package datastore
 
 import (
-	"database/sql"
+	"context"
 	"io"
-	"net"
-	"net/url"
-	"strings"
+	"os"
 	"time"
 
+	"github.com/jackc/pgx/v4/pgxpool"
+	// "github.com/jackc/pgx/v4/pgxpool"
 	_ "gopkg.in/mattes/migrate.v1/driver/postgres" //for migrations
-	"gopkg.in/mattes/migrate.v1/migrate"
-
-	dat "github.com/nerdynz/dat/dat"
-	runner "github.com/nerdynz/dat/sqlx-runner"
 )
-
-type Websocket interface {
-	Broadcast(string, string) error
-}
 
 // Logger - designed as a drop in for logrus with some other backwards compat stuff
 type Logger interface {
@@ -37,10 +29,9 @@ type Logger interface {
 }
 
 type Datastore struct {
-	DB          *runner.DB
+	DB          *pgxpool.Pool
 	Cache       Cache
 	Settings    Settings
-	Websocket   Websocket
 	Logger      Logger
 	FileStorage FileStorage
 }
@@ -61,130 +52,53 @@ type Cache interface {
 	GetBytes(key string) ([]byte, error)
 	SetBytes(key string, value []byte, duration time.Duration) error
 	FlushDB() error
+	// Close() error
 }
 
 func (ds *Datastore) TurnOnLogging() {
-	dat.SetDebugLogger(ds.Logger.Warnf)
-	dat.SetSQLLogger(ds.Logger.Infof)
-	dat.SetErrorLogger(ds.Logger.Errorf)
+	// dat.SetDebugLogger(ds.Logger.Warnf)
+	// dat.SetSQLLogger(ds.Logger.Infof)
+	// dat.SetErrorLogger(ds.Logger.Errorf)
 }
 
 func (ds *Datastore) TurnOffLogging() {
-	dat.SetDebugLogger(nil)
-	dat.SetSQLLogger(nil)
-	dat.SetErrorLogger(nil)
+	// dat.SetDebugLogger(nil)
+	// dat.SetSQLLogger(nil)
+	// dat.SetErrorLogger(nil)
 }
 
 type FileStorage interface {
 	OpenFile(fileIdentifier string) (b []byte, fileid string, fullURL string, err error)
 	GetURL(fileIdentifier string) (fullURL string)
-	// always returning bytes might be a little expensive, but it makes the interface much more reasonable
 	SaveFile(fileIdentifier string, b io.Reader, sanitizePath bool) (fileid string, fullURL string, err error)
 }
 
 // New - returns a new datastore which contains redis, database and settings.
 // everything in the datastore should be concurrent safe and stand within thier own right. i.e. accessible at anypoint from the app
-func New(logger Logger, settings Settings, cache Cache, filestorage FileStorage, ws Websocket) *Datastore {
-	store := Simple()
+func New(logger Logger, settings Settings, cache Cache, filestorage FileStorage) *Datastore {
+	store := &Datastore{}
 	store.Logger = logger
 	store.Settings = settings
-	store.DB = getDBConnection(store, cache)
 	store.Cache = cache
 	store.FileStorage = filestorage
+
+	// conf, err := pgx.ParseConfig()
+	// if err != nil {
+	// 	store.Logger.Error(err)
+	// 	os.Exit(1)
+	// }
+	// db, _ := pgxpool.Connect(ctx, "example-connection-url")``
+	conn, err := pgxpool.Connect(context.Background(), store.Settings.Get("DATABASE_URL"))
+	if err != nil {
+		store.Logger.Error(err)
+		os.Exit(1)
+	}
+
+	store.DB = conn
 	return store
 }
 
 func (ds *Datastore) Cleanup() {
-	ds.DB.DB.Close()
-	// ds.Cache.Client.Close()
-}
-
-func Simple() *Datastore {
-	store := &Datastore{}
-	return store
-}
-
-func getDBConnection(store *Datastore, cache Cache) *runner.DB {
-	//get url from ENV in the following format postgres://user:pass@192.168.8.8:5432/spaceio")
-	dbURL := store.Settings.Get("DATABASE_URL")
-	u, err := url.Parse(dbURL)
-	if err != nil {
-		store.Logger.Error(err)
-	}
-
-	username := u.User.Username()
-	pass, isPassSet := u.User.Password()
-	if !isPassSet {
-		store.Logger.Error("no database password")
-	}
-	host, port, _ := net.SplitHostPort(u.Host)
-	dbName := strings.Replace(u.Path, "/", "", 1)
-
-	if host == "GCLOUD_SQL_INSTANCE" {
-		// USE THE GCLOUD_SQL_INSTANCE SETTING instead... e.g. host= /cloudsql/INSTANCE_CONNECTION_NAME
-		host = store.Settings.Get("GCLOUD_SQL_INSTANCE")
-	}
-
-	dbStr := "dbname=" + dbName + " user=" + username + " host=" + host
-	if port != "" {
-		dbStr += " port=" + port
-	}
-	store.Logger.Info(dbStr)
-
-	db, _ := sql.Open("postgres", dbStr+" password="+pass+" sslmode=disable ") // pass goes last
-	err = db.Ping()
-	if err != nil {
-		store.Logger.Error(err)
-		panic(err)
-	}
-	store.Logger.Info("database running")
-	// ensures the database can be pinged with an exponential backoff (15 min)
-	runner.MustPing(db)
-
-	// if store.Settings.Get("CACHE_NAMESPACE") != "" {
-	// 	redisUrl := ":6379"
-	// 	if store.Settings.Get("CACHE_URL") != "" {
-	// 		redisUrl = store.Settings.Get("CACHE_URL")
-	// 	}
-	// 	cache, err := kvs.NewRedisStore(store.Settings.Get("CACHE_NAMESPACE"), redisUrl, "")
-	// 	if err != nil {
-	// 		store.Logger.Error(err)
-	// 		panic(err)
-	// 	}
-	// 	store.Logger.Info("USING CACHE", store.Settings.Get("CACHE_NAMESPACE"))
-	// 	runner.SetCache(cache)
-	// }
-	if cache != nil {
-		runner.SetCache(cache)
-	}
-
-	// set to reasonable values for production
-	db.SetMaxIdleConns(4)
-	db.SetMaxOpenConns(16)
-
-	// set this to enable interpolation
-	dat.EnableInterpolation = true
-
-	if store.Settings.IsProduction() {
-		// PRODUCTION
-		errs, ok := migrate.UpSync(dbURL+"?sslmode=disable", "./server/models/migrations")
-		if !ok {
-			finalError := ""
-			for _, err := range errs {
-				finalError += err.Error() + "\n"
-			}
-			store.Logger.Error(finalError)
-		}
-	} else {
-		// DEV`
-		// set to check things like sessions closing.
-		// Should be disabled in production/release builds.
-		dat.Strict = true
-
-		// Log any query over 10ms as warnings. (optional)
-		// runner.LogQueriesThreshold = 1 * time.Microsecond // LOG EVERYTHING ON DEV // turn it off and on with a flag
-	}
-
-	// db connection
-	return runner.NewDB(db, "postgres")
+	ds.DB.Close()
+	// ds.Cache.Close()
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.16
 
 require (
 	github.com/cznic/ql v1.2.0 // indirect
-	github.com/nerdynz/dat v1.2.0
+	github.com/jackc/pgx/v4 v4.13.0
+	github.com/lib/pq v1.10.3 // indirect
+	github.com/nerdynz/dat v1.3.0
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	gopkg.in/mattes/migrate.v1 v1.3.2
 	gopkg.in/stretchr/testify.v1 v1.2.2 // indirect
